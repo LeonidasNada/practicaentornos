a5032b03c21332d7b7fe06af837b171113fba169
Bryan <alumno@f0202>
2021-07-01 20:35:06 +0200

    Creacion de las clases de prueba

---
+++ ProyectoEntornos/src/AdministradorTest.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AdministradorTest {
+	private boolean autenticacion;
+	private String nombreUsuario;
+	private Administrador administrador;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testLogin() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testComprobacionLogin() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testCrearAdmin() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testEliminarAdmin() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testModificarAdmin() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testVerAdmins() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
---
+++ ProyectoEntornos/src/PlantillaTest.java
@@ -0,0 +1,50 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlantillaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testMenuPlantilla() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testVerPlantilla() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testVerIdPlantillas() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testEliminarPlantilla() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testModificarPlantilla() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testCrearPlantilla() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testRecuperarIdPlantilla() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testObtNombreJugador() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
